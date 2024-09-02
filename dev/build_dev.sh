a4cd560d917d6d1c7211cb6aef13cc5fcb73b807
lypavel <157053921+lypavel@users.noreply.github.com>
2024-09-02 15:43:15 +0000

    new file structure, dockerfile for development environment

---
+++ dev/build_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker-compose down -v
+
+docker-compose up --build -d
+
+echo 'Waiting for database'
+sleep 5
+
+until docker-compose exec backend python manage.py migrate --noinput; do
+    echo 'Looks like database is not initialized yet. Retrying...'
+    sleep 5
+done
+
+echo 'Restarting containers to fix network issues'
+sleep 5
+
+docker-compose restart
+
+echo 'Done!'
